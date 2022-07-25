44dfb60bdda39c205107933bbbe4449aae4f08b6
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-07-24 23:27:33 -0400

    added more content and pictures

--- js/home.js
+++ js/home.js
@@ -9,6 +9,17 @@ export default function home(){
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic harum voluptates provident adipisci blanditiis atque quos sunt aperiam laborum temporibus, ipsum omnis 
             quibusdam vel recusandae officiis placeat, molestiae similique minima?</p>
         </div>
+        <div class="secDiv">
+            <div class="wedDiv">
+            <h2>WEDDINGS</h2>
+            </div>
+            <div class="cupDiv">
+            <h2>CUPCAKES</h2>
+            </div>
+            <div class="otherDiv">
+            <h2>OCCASION CAKES</h2>
+            </div>
+        </div>
     </main>
     `;
 }
